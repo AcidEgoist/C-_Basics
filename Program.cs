a909c7d8a4213131aec4386de9e35546a64475ab
AcidEgoist <mainegoist@yandex.ru>
2022-05-08 19:13:06 +0300

    Added Profiler func

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Profiler();
+
+static void Profiler(){
+
+    string firstName, lastName;
+    int age;
+    float weight, height;
+
+    //Clearing console before using
+    Console.Clear();
+
+    //Reading information from user's console
+    Console.WriteLine("Введите Ваше имя: ");
+    firstName = Console.ReadLine();
+    Console.Clear();
+
+    Console.WriteLine("Введите Вашу фамилию: ");
+    lastName = Console.ReadLine();
+    Console.Clear();
+
+    Console.WriteLine("Введите Ваш возраст: ");
+    int.TryParse(Console.ReadLine(), out age);
+    Console.Clear();
+
+    Console.WriteLine("Введите Ваш рост: ");
+    float.TryParse(Console.ReadLine(), out height);
+    Console.Clear();
+
+    Console.WriteLine("Введите Ваш вес: ");
+    float.TryParse(Console.ReadLine(), out weight);
+    Console.Clear();
+
+    //Visualisation of profile
+    Console.WriteLine("Проверка правильности данных.\nИмя: {0};\nФамилия: {1};\nВозраст: {2};\nРост: {3};\nВес: {4}.", firstName, lastName, age, height, weight);
+
+}
